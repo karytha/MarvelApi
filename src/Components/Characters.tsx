87392fa4adc5221467af6f4fe38b4721f2f24465
Kárytha de Paula <karythanascimento@gmail.com>
2019-04-26 16:34:11 -0300

    Adicionando e removendo favoritos

--- src/Components/Characters.tsx
+++ src/Components/Characters.tsx
@@ -1,61 +1,72 @@
 import React, { Component } from "react";
 import "../CSS/Home.css";
 
-
 type Props = {
-    comic: any;
-}
-
-export default class Characters extends Component<Props>{
-
-    state = {
-        showMoreInfo: false,
-        checked: false
-    }
-
-
-    onShowMoreInfo = () => {
-        this.setState({ showMoreInfo: !this.state.showMoreInfo });
-    };
-
-    onChecked = () => {
-        this.setState({ checked: this.state.checked })
-
-        console.log(this.state.checked)
-    }
-
-
-    render() {
-
-        const { comic } = this.props;
-        const { showMoreInfo } = this.state;
-
-        return (
-            <div>
-                <div key={comic.id}>
-                    <div className="card" >
-                        <div className="container">
-                            <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt="Avatar" />
-                            <h4>{comic.name}</h4>
-                            <input id="checkbox" type="checkbox" className="checkbox" onChange={this.onChecked} />
-
-                            {showMoreInfo === true ? (
-                                <>
-                                    <p>{comic.description}</p>
-                                    <p>{comic.modified}</p>
-                                    <p>comics: {comic.comics.available} </p>
-                                    <p> series: {comic.series.available}</p>
-
-                                </>
-                            ) : null}
-                            <button onClick={this.onShowMoreInfo}> Ver mais..</button>
-
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        );
-
-    }
+  comic: any;
 };
+
+export default class Characters extends Component<Props> {
+  state = {
+    showMoreInfo: false,
+    checked: false
+  };
+
+  addToFavorites = (comicId: any, comicInfo: any) => {
+    localStorage.setItem(comicId, JSON.stringify(comicInfo));
+  };
+
+  removeFromFavorites = (comicId: any) => {
+    localStorage.removeItem(comicId);
+  };
+
+  onChecked = (comic: any) => {
+    this.setState({ checked: !this.state.checked }, () => {
+      if (this.state.checked) {
+        this.addToFavorites(comic.id, comic);
+      } else {
+        this.removeFromFavorites(comic.id);
+      }
+    });
+  };
+
+  onShowMoreInfo = () => {
+    this.setState({ showMoreInfo: !this.state.showMoreInfo });
+  };
+
+  render() {
+    const { comic } = this.props;
+    const { showMoreInfo } = this.state;
+
+    return (
+      <div>
+        <div key={comic.id}>
+          <div className="card">
+            <div className="container">
+              <img
+                src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+                alt="Avatar"
+              />
+              <h4>{comic.name}</h4>
+
+              <input
+                id="checkbox"
+                type="checkbox"
+                className="checkbox"
+                onChange={() => this.onChecked(comic)}
+              />
+
+              {!!showMoreInfo ? (
+                <>
+                  <p>{comic.description}</p>
+                  <p>comics: {comic.comics.available} </p>
+                  <p> series: {comic.series.available}</p>
+                </>
+              ) : null}
+              <button onClick={this.onShowMoreInfo}> Ver mais..</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -2,54 +2,60 @@ import React, { Component, useState } from "react";
 import Header from "../Components/Header";
 import "../CSS/Home.css";
 import Bottom from "../Components/Bottom";
-import Characters from "../Components/Characters"
+import Characters from "../Components/Characters";
 
 export default class Home extends Component {
+  state = {
+    comics: [],
+    search: " ",
+    Open: false,
+    favorited: false
+  };
 
+  async fetchCharacters(search: string) {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${search}&ts=1&apikey=229d8edde9c1cde400c427df49959956&hash=9bf04075fdccf819051d6fc49e30ff58`
+    );
+    const responseJson = await response.json();
+    this.setState({ comics: responseJson.data.results });
+  }
 
-    state = {
-        comics: [],
-        search: " ",
-        Open: false,
-        favorited: false
-    }
+  updateSearch = (e: any) => {
+    console.log(e.target.value);
+    this.setState({ search: e.target.value });
+    // debounce(this.fetchCharacters(this.state.search));
+    this.fetchCharacters(this.state.search);
+  };
 
+  render() {
+    const { comics } = this.state;
 
-    async fetchCharacters(search: any) {
+    return (
+      <div>
+        <Header title="marvel" button="veja mais" />
 
-        const response = await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${search}&ts=1&apikey=229d8edde9c1cde400c427df49959956&hash=9bf04075fdccf819051d6fc49e30ff58`)
-        const responseJson = await response.json()
-        this.setState({ comics: responseJson.data.results })
-        // console.log(hash);
-    }
+        <input
+          type="text"
+          className="text"
+          placeholder="Por qual herói você procura?"
+          onChange={this.updateSearch}
+        />
 
-
-    updateSearch = (e: any) => {
-        console.log(e.target.value);
-        this.setState({ search: e.target.value })
-        this.fetchCharacters(this.state.search)
-
-    };
-
-
-    render() {
-        const { comics } = this.state;
-
-        return (
-            <div>
-                <Header title="marvel" button="veja mais" />
-
-                <input type="text" className="text" placeholder="Por qual herói você procura?" onChange={this.updateSearch}></input>
-
-                <body className="body">
-                    {comics.map(comic => {
-                        return (
-                            <Characters comic={comic} />)
-                    })}
-                </body>
-                <Bottom title="MARVEL" />
-
-            </div>
-        )
-    }
+        <div className="body">
+          {comics.map(comic => {
+            return <Characters comic={comic} />;
+          })}
+        </div>
+        <Bottom title="MARVEL" />
+      </div>
+    );
+  }
 }
+
+// function debounce(func: () => void, wait = 50) {
+//   let h: any;
+//   return () => {
+//     clearTimeout(h);
+//     h = setTimeout(() => func(), wait);
+//   };
+// }
